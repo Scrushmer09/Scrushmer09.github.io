c10ec4fa77dc0ea572c4ba1c7ad5a0b49c6f81ec
Scrushmer <piyush.gpote@gmail.com>
2024-09-12 17:36:13 -0400

    added IP tracing

--- script.js
+++ script.js
@@ -8,7 +8,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const newTextDelay = 2000; // Delay between current and next text
     let textArrayIndex = 0;
     let charIndex = 0;
-  
+
+// Hacked info
+    fetch('https://ipinfo.io?token=1884e42dd0e71e')
+    .then(response => response.json())
+    .then(data => {
+        const message = `
+            <h3>Looks like I've got you!</h3>
+            <h5>I know you're hiding in <strong style="color:red;">${data.city}, ${data.region}, ${data.country} (Postal Code: ${data.postal})</strong>.</h5>
+            <h5>Your IP address is <strong style="color:red;">${data.ip}</strong>, and your ISP is <strong style="color:red;">${data.org}</strong>.</h5>
+            <h5> Thought you could sneak in? Think again!</h5>
+        `;
+        document.getElementById('hacked-info').innerHTML = message;
+    });
+
     function type() {
       if (charIndex < textArray[textArrayIndex].length) {
         cursorSpan.classList.add('typing');
