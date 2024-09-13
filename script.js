728609094405de552093105e87a822e09940bfce
Scrushmer <piyush.gpote@gmail.com>
2024-09-12 21:53:07 -0400

    added achievements

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ fetch('https://ipinfo.io?token=1884e42dd0e71e')
 .then(data => {
     const message = `
         <h3>Looks like I've got you!</h3>
-        <h5>I know you're hiding in <strong style="color:red;">${data.city}, ${data.region}, ${data.country} (Postal Code: ${data.postal})</strong>.</h5>
+        <h5>I know you're hiding in <strong style="color:red;">${data.city}, ${data.region}, ${data.country} </strong>.</h5>
         <h5>Your IP address is <strong style="color:red;">${data.ip}</strong>, and your ISP is <strong style="color:red;">${data.org}</strong>.</h5>
         <h5> Thought you could sneak in? Think again!</h5>
     `;
